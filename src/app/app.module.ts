65063b66984c15cf7a94714e775be05c001bbd6c
Keyner Barrios <barrioskeyner693@gmail.com>
2024-04-15 13:32:32 -0500

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/components/counter/counter.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HereosModule } from './heroes/heros.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HereosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
